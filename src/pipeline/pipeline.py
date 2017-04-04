1e090cb9a7391a6e70f2facfe746e81d57e2f0bf
bkushigian <redundnt@gmail.com>
2017-04-04 10:52:08 -0400

    added pipeline.py, wrote class stub

---
+++ src/pipeline/pipeline.py
@@ -0,0 +1,13 @@
+'''
+pipeline.py
+define the Pipeline class
+Date:   4/4/17
+Author: Benjamin Kushigian
+
+TODOs: 
+    * Just about everything
+'''
+
+class Pipeline(object):
+    def __init__(self):
+        pass
