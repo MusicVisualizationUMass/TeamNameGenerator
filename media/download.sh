db76cf4f064f775a71b6d5556b43e0d6e38afb58
Ben Kushigian <bkushigian@umass.edu>
2017-04-04 19:51:06 -0400

    Updated gitignore to ignore audio files; included media/download.sh to download smaple mp3 files

---
+++ media/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget benkushigian.com/media/TicoTico.mp3
+wget benkushigian.com/media/StuffnStuff.mp3
+wget benkushigian.com/media/Noish.mp3
+wget benkushigian.com/media/WesternWaltz.mp3
