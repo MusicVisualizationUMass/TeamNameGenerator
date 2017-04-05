e5541f9797fdcb9b09de5b38c62fe8cc513ef273
Ben Kushigian <bkushigian@umass.edu>
2017-04-04 22:31:03 -0400

    Updated download.sh

--- media/download.sh
+++ media/download.sh
@@ -4,3 +4,6 @@ wget benkushigian.com/media/TicoTico.mp3
 wget benkushigian.com/media/StuffnStuff.mp3
 wget benkushigian.com/media/Noish.mp3
 wget benkushigian.com/media/WesternWaltz.mp3
+wget benkushigian.com/media/Wanks1.wav
+wget benkushigian.com/media/WyRm.wav
+wget benkushigian.com/media/swing.wav
