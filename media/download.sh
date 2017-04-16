b94d4798f15826bdc412908d55b58d85fe96cc28
bkushigian <redundnt@gmail.com>
2017-04-16 11:26:10 -0400

    Added test audio; updated download.sh

--- media/download.sh
+++ media/download.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-wget benkushigian.com/media/TicoTico.mp3
-wget benkushigian.com/media/StuffnStuff.mp3
-wget benkushigian.com/media/Noish.mp3
-wget benkushigian.com/media/WesternWaltz.mp3
-wget benkushigian.com/media/Wanks1.wav
-wget benkushigian.com/media/WyRm.wav
-wget benkushigian.com/media/swing.wav
+wget "benkushigian.com/media/TicoTico.mp3"
+wget "benkushigian.com/media/StuffnStuff.mp3"
+wget "benkushigian.com/media/Noish.mp3"
+wget "benkushigian.com/media/WesternWaltz.mp3"
+wget "benkushigian.com/media/Wanks1.wav"
+wget "benkushigian.com/media/WyRm.wav"
+wget "benkushigian.com/media/swing.wav"
+wget "benkushigian.com/media/mvp/A440_Against_770.mp3"
+wget "benkushigian.com/media/mvp/A440_Against_770.wav"
+wget "benkushigian.com/media/mvp/A440.mp3"
+wget "benkushigian.com/media/mvp/A440_With_E660_Pulse.mp3"
