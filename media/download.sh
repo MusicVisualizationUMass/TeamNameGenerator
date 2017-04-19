c9339dda6919b511a7bf555b9f8fcd910169df26
bkushigian <redundnt@gmail.com>
2017-04-19 13:37:51 -0400

    Updated download.sh

--- media/download.sh
+++ media/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm *.wav
+rm *.mp3
 wget benkushigian.com/media/TicoTico.mp3
 wget benkushigian.com/media/StuffnStuff.mp3
 wget benkushigian.com/media/Noish.mp3
@@ -7,3 +9,7 @@ wget benkushigian.com/media/WesternWaltz.mp3
 wget benkushigian.com/media/Wanks1.wav
 wget benkushigian.com/media/WyRm.wav
 wget benkushigian.com/media/swing.wav
+wget benkushigian.com/media/mvp/2200Hz_4522.wav
+wget benkushigian.com/media/mvp/440Hz_With_660Hz-Pulse.wav
+wget benkushigian.com/media/mvp/Silence.wav
+wget benkushigian.com/media/mvp/StuffnStuff.wav
