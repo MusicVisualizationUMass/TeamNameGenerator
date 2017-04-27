a90a120bc9c4d638de2279aa63e18f55925d190e
bkushigian <redundnt@gmail.com>
2017-04-27 11:49:37 -0400

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup( name         = 'Music Visualizer UMass'
+     , version      = '1.0'
+     , description  = 'Visualizer for mp3s'
+     , author       = 'Fedore Arkhipov, Ben Guinsburg, Ben Kushigian, Thomas Peck, Sam Remis, Ivan Siryk'
+     , author_email = 'bkushigian@gmail.com'
+     , packages     = ['pipeline', 'ui', 'test']
+     , package_dir =  { 'pipeline' : 'src/pipeline'
+                      , 'ui'       : 'src/ui'
+                      , 'test'     : 'src/test'
+                      }
+     )
