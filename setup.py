45879eee8fc40c42fa8771feeb94b18f1fba83db
bkushigian <redundnt@gmail.com>
2017-04-27 21:12:11 -0400

    Updated setup.py for clean and nice numpy install. Untested!

--- setup.py
+++ setup.py
@@ -2,12 +2,39 @@
 
 try:
     from setuptools import setup
+    from setuptools.command.build_ext import build_ext as _build_ext
 except:
-    print("[!] Warning: unable to import setuptools.setup. Attempting to continue with distutils.core.setup")
-    from distutils.core import setup
+    print(
+'''
+[!] Warning: unable to import setuptools.setup. Please visit your package
+manager and install pip3 (or setuptools for python3, thought this hasn't been
+tested. On Debian-based Linux distros it will be
+
+    $ sudo apt-get install python3-pip
+
+and on Mac OS run
+
+    $ brew install pip3       # Double check this - I'm just guessing :)
+
+As always, good luck and happy visualizing!
+'''
+)
+
+# XXX: This is from a stack exchange post and should be inspected at some point
+# when someone has time/gumption. For more info, checkout:
+# http://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is sill in its setup process
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 
 setup( name         = 'musicvisualizer'
+     , cmdclass     = {'build_ext':build_ext}
      , version      = '1.0'
      , description  = 'Visualizer for mp3s'
      , author       = 'Fedore Arkhipov, Ben Guinsburg, Ben Kushigian, Thomas Peck, Sam Remis, Ivan Siryk'
